6586819440ba59bc9025a195c890e6bdf3af1889
Oğuzcan Genç <mail@oguzcangenc.com>
2021-02-24 20:44:38 +0300

    Autofac implementasyonu kodlandı.

--- WebAPI/Controllers/CarController.cs
+++ WebAPI/Controllers/CarController.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
@@ -11,5 +7,33 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CarController : ControllerBase
     {
+        ICarService _carService;
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getcardetails")]
+
+        public IActionResult GetCarDetails()
+        {
+            var result = _carService.GetCarDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest(result.Message);
+        }
     }
 }
