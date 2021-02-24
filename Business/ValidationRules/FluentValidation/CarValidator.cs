37588661cf8b272c76fbafad08753fe17728eb89
Oğuzcan Genç <mail@oguzcangenc.com>
2021-02-24 20:51:55 +0300

    FluentValidation İmplementasyonu kodlandı.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor<>
+        }
+    }
+}
