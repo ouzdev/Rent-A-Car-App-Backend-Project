d81b8efdcabf0ff8bf51244472fe560800167761
Oğuzcan Genç <mail@oguzcangenc.com>
2021-04-03 23:53:06 +0300

    The Cache and Secured Operation aspects are encoded in the User manager class 🚀

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.Entities.Concrete;
 using Core.Utilities.Result;
@@ -16,32 +18,21 @@ namespace Business.Concrete
         {
             _userDal = userDal;
         }
-
-        [ValidationAspect(typeof(UserValidator))]
-        public IResult Add(User user)
-        {
-            _userDal.Add(user);
-            return new SuccessResult(Messages.UserAdded);
-        }
-
-        public IResult Delete(User user)
-        {
-            _userDal.Delete(user);
-            return new SuccessResult(Messages.UserDeleted);
-        }
+        [CacheAspect(duration: 10)]
 
         public IDataResult<List<User>> GetAll()
         {
             var result = _userDal.GetAll();
             return new SuccessDataResult<List<User>>(result, Messages.UserListed);
         }
+        [CacheAspect(duration: 10)]
 
         public IDataResult<User> GetById(int id)
         {
             var result = _userDal.Get(u => u.Id == id);
             return new SuccessDataResult<User>(result, Messages.UserListed);
         }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<User> GetByMail(string mail)
         {
             User user = _userDal.Get(p => p.Email == mail);
@@ -54,17 +45,34 @@ namespace Business.Concrete
                 return new SuccessDataResult<User>(user, Messages.SuccessGetByUserMail);
             }
         }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<List<OperationClaim>> GetClaims(User user)
         {
             return new SuccessDataResult<List<OperationClaim>>(_userDal.GetClaims(user));
         }
-
+        [SecuredOperation("user.update,administrator")]
         [ValidationAspect(typeof(UserValidator))]
+        [CacheRemoveAspect("IUserService.Get")]
         public IResult Update(User user)
         {
             _userDal.Update(user);
             return new SuccessResult(Messages.UserUpdated);
         }
+        [SecuredOperation("user.add,administrator")]
+        [ValidationAspect(typeof(UserValidator))]
+        [CacheRemoveAspect("IUserService.Get")]
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult(Messages.UserAdded);
+        }
+        [SecuredOperation("user.delete,administrator")]
+        [CacheRemoveAspect("IUserService.Get")]
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult(Messages.UserDeleted);
+        }
+
     }
 }
