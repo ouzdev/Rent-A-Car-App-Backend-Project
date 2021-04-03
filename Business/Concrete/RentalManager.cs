f02ac30cb91bead6fb78997483de9e4d54142dbb
Oğuzcan Genç <mail@oguzcangenc.com>
2021-04-03 23:50:48 +0300

    The Cache and Secured Operation aspects are encoded in the Rental manager class 🚀

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Result;
 using DataAccess.Abstract;
@@ -17,38 +19,20 @@ namespace Business.Concrete
         {
             _rentalDal = rentalDal;
         }
-
-        [ValidationAspect(typeof(RentalValidator))]
-        public IResult Add(Rental rental)
-        {
-            if (IsCarAvailable(rental.CarId).Success)
-            {
-                _rentalDal.Add(rental);
-                return new SuccessResult(Messages.RentalAdded);
-            }
-            return new ErrorResult(Messages.RentalCarNotAvailable);
-
-        }
-
-        public IResult Delete(Rental rental)
-        {
-            _rentalDal.Delete(rental);
-            return new SuccessResult(Messages.RentalDeleted);
-        }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Rental>> GetAll()
         {
             var result = _rentalDal.GetAll();
             return new SuccessDataResult<List<Rental>>(result, Messages.RentalListed);
         }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<Rental> GetById(int id)
         {
             var result = _rentalDal.Get(r => r.Id == id);
             return new SuccessDataResult<Rental>(result, Messages.RentListed);
 
         }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<List<GetRentalDetailDTO>> GetListRentalDetails()
         {
             var result = _rentalDal.GetRentalDetails();
@@ -66,12 +50,33 @@ namespace Business.Concrete
             return new ErrorResult(Messages.RentalCarNotAvailable);
 
         }
-
+        [SecuredOperation("rental.add,administrator")]
         [ValidationAspect(typeof(RentalValidator))]
+        [CacheRemoveAspect("IRentalService.Get")]
+        public IResult Add(Rental rental)
+        {
+            if (IsCarAvailable(rental.CarId).Success)
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult(Messages.RentalAdded);
+            }
+            return new ErrorResult(Messages.RentalCarNotAvailable);
+
+        }
+        [SecuredOperation("rental.update,administrator")]
+        [ValidationAspect(typeof(RentalValidator))]
+        [CacheRemoveAspect("IRentalService.Get")]
         public IResult Update(Rental rental)
         {
             _rentalDal.Update(rental);
             return new SuccessResult(Messages.RentalUpdated);
+        }
+        [SecuredOperation("rental.delete,administrator")]
+        [CacheRemoveAspect("IRentalService.Get")]
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult(Messages.RentalDeleted);
         }
     }
 }
