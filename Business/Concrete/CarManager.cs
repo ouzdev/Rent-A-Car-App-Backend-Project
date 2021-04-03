04c25dd9a791ffdd75810bbffcfe66d783fe6b75
Oğuzcan Genç <mail@oguzcangenc.com>
2021-04-03 22:46:25 +0300

    The Cache and Secured Operation aspects are encoded in the Car manager class 🚀

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -2,6 +2,7 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Result;
@@ -19,38 +20,28 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-        [SecuredOperation("product.add")]
-        [ValidationAspect(typeof(CarValidator))]
-        public IResult Add(Car car)
-        {
-            ValidationTool.Validate(new CarValidator(), car);
-            _carDal.Add(car);
-            return new SuccessResult(Messages.CarAdded);
-        }
 
-        public IResult Delete(Car car)
-        {
-            _carDal.Delete(car);
-            return new SuccessResult(Messages.CarDeleted);
-        }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Car>> GetAll()
         {
             var result = _carDal.GetAll();
             return new SuccessDataResult<List<Car>>(result, Messages.CarListed);
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<Car> GetById(int id)
         {
             return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == id));
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<List<CarDetailsDTO>> GetCarDetails()
         {
             var result = _carDal.GetCarDetails();
             return new SuccessDataResult<List<CarDetailsDTO>>(result, Messages.GetCarDetails);
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Car>> GetCarsByBrandId(int id)
         {
             var result = _carDal.GetAll(c => c.BrandId == id);
@@ -58,17 +49,37 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Car>>(result, Messages.GetCarsBrandId);
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Car>> GetCarsByColorId(int id)
         {
             var result = _carDal.GetAll(c => c.ColorId == id);
             return new SuccessDataResult<List<Car>>(result, Messages.GetCarsColorId);
         }
 
+        [SecuredOperation("car.update,administrator")]
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Update(Car car)
         {
             _carDal.Update(car);
             return new SuccessResult(Messages.CarUpdated);
+        }
+
+        [SecuredOperation("car.add,administrator")]
+        [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarService.Get")]
+        public IResult Add(Car car)
+        {
+            ValidationTool.Validate(new CarValidator(), car);
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
+        }
+        [SecuredOperation("car.add,administrator")]
+        [CacheRemoveAspect("ICarService.Get")]
+        public IResult Delete(Car car)
+        {
+            _carDal.Delete(car);
+            return new SuccessResult(Messages.CarDeleted);
         }
     }
 }
