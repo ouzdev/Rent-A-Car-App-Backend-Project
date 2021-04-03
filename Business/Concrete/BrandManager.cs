8c50c0db6b51965c8d2fa476bcf262cf485b85ef
Oğuzcan Genç <mail@oguzcangenc.com>
2021-04-03 22:14:29 +0300

    The Cache and Secured Operation aspects are encoded in the Brand manager class 🚀

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Result;
 using DataAccess.Abstract;
@@ -17,36 +19,45 @@ namespace Business.Concrete
             _brandDal = brandDal;
         }
 
-        [ValidationAspect(typeof(BrandValidator))]
-        public IResult Add(Brand brand)
-        {
-            _brandDal.Add(brand);
-            return new SuccessResult(Messages.BrandAdded);
-        }
-
-        public IResult Delete(Brand brand)
-        {
-            _brandDal.Delete(brand);
-            return new SuccessResult(Messages.BrandDeleted);
-        }
-
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Brand>> GetAll()
         {
             var result = _brandDal.GetAll();
             return new SuccessDataResult<List<Brand>>(result);
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<Brand> GetById(int id)
         {
             var result = _brandDal.Get(x => x.Id == id);
             return new SuccessDataResult<Brand>(result);
         }
 
+        [SecuredOperation("brand.update,administrator")]
         [ValidationAspect(typeof(BrandValidator))]
+        [CacheRemoveAspect("IBrandService.Get,administrator")]
         public IResult Update(Brand brand)
         {
             _brandDal.Update(brand);
             return new SuccessResult(Messages.BrandUpdated);
         }
+
+        [SecuredOperation("brand.delete,administrator")]
+        [CacheRemoveAspect("IBrandService.Get")]
+        public IResult Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+            return new SuccessResult(Messages.BrandDeleted);
+        }
+
+        [ValidationAspect(typeof(BrandValidator))]
+        [SecuredOperation("brand.add,administrator")]
+        [CacheRemoveAspect("IBrandService.Get")]
+        public IResult Add(Brand brand)
+        {
+            _brandDal.Add(brand);
+            return new SuccessResult(Messages.BrandAdded);
+        }
+
     }
 }
