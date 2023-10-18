6769668bce44e4f7ab42fe7d3e51d4c0c9835347
Ajer Lodhi <lodhia@seattleu.edu>
2023-10-19 00:00:05 +0500

    Added unit test files for the controller in WebAPI

---
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
@@ -0,0 +1,11 @@
+namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
+{
+    [TestClass]
+    public class ColorsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
