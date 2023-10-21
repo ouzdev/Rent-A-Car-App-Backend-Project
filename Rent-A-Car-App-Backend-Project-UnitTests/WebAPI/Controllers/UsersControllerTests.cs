4515f1f0658199ba7baef7137ec940273d42829e
Anusha Panta <apanta@seattleu.edu>
2023-10-21 14:50:30 -0700

    2 test cases for Add User -> UsersControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/UsersControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/UsersControllerTests.cs
@@ -1,11 +1,64 @@
+using Moq;
+using Business.Abstract;
+using Core.Utilities.Result;
+using Core.Entities.Concrete;
+
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
 namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
 {
+
     [TestClass]
-    public class UsersControllerTests
+    public class UserServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_ReturnsSuccessResult()
         {
+            // Arrange
+            var mockUserRepository = new Mock<IUserService>();
+            var mockResult = new Mock<IResult>();
+            mockResult.Setup(r => r.Success).Returns(true);
+            mockResult.Setup(r => r.Message).Returns("New User is added successfully");
+
+            mockUserRepository.Setup(ur => ur.Add(It.IsAny<User>())).Returns(mockResult.Object);
+
+            var validUser = new User { FirstName = "Anusha panta", LastName = "Panta" };
+
+            // Act
+            var result = mockUserRepository.Object.Add(validUser);
+
+            // Assert
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual("User added successfully", result.Message);
+        }
+
+        [TestMethod]
+        public void AddData()
+        {
+            var User = new User
+            {
+                Id = 1,
+                FirstName = "Anusha",
+                LastName = "Panta",
+                Email = "test@gmail.com",
+                Status = false
+            };
+
+            User.Status = true;
+
+            Assert.IsTrue(User.Status);
+
         }
     }
 }
+
+
+
+
+
+    
+    
+    
+ 
