0e5d6a9085892712d386c44e39cdba57a947ad3b
kaiservanguardia <thegiankaiser@gmail.com>
2023-10-22 14:04:56 -0700

    UpdateColorsControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
@@ -3,9 +3,22 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
     [TestClass]
     public class ColorsControllerTests
     {
+       
         [TestMethod]
-        public void TestMethod1()
+        public void Test_GetBy_Id()
         {
+            // Arrange
+            var mockColorService = new Mock<IColorService>();
+            mockColorService.Setup(x => x.GetById(It.IsAny<int>())).Returns(new ColorResponse(true, "Success", new Color()));
+
+            var controller = new ColorsController(mockColorService.Object);
+
+            // Act
+            var result = controller.GetById(1) as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+           
         }
     }
 }
