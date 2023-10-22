a0f9b91f76fa3d74c30b4a7e5d0d6ecf947b3674
Kaiser Vanguardia <122261201+thegiankaiser@users.noreply.github.com>
2023-10-22 16:52:15 -0700

    Update ColorsControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using WebAPI.Controllers;
+
+namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers;
 {
     [TestClass]
     public class ColorsControllerTests
