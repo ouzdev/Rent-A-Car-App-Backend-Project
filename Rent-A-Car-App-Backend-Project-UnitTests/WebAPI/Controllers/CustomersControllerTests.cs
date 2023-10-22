d17a74dcac411aa1be076642882e2879299cb1a1
Kaiser Vanguardia <122261201+thegiankaiser@users.noreply.github.com>
2023-10-22 16:50:31 -0700

    Update CustomersControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/CustomersControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/CustomersControllerTests.cs
@@ -1,3 +1,12 @@
+using Business.Abstract;
+using Core.Utilities.Result;
+using Core.Utilities.Security.JWT;
+using Entities.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers;
+using Moq;
+using Core.Entities.Concrete;
+
 namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
 {
     [TestClass]
