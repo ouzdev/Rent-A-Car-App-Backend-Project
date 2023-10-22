31a3163dbd7b18880bbfaeb4f857983892852ebc
Kaiser Vanguardia <122261201+thegiankaiser@users.noreply.github.com>
2023-10-22 16:48:53 -0700

    Update CustomersControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/CustomersControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/CustomersControllerTests.cs
@@ -4,8 +4,23 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
     public class CustomersControllerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetById()
         {
+            // Arrange
+            var mockCustomer = new Mock<ICustomer>();
+            var cust = new Cust { UserId = 1, CompanyName = "Company A" };
+            mockCustomer.Setup(x => x.Get(c => c.UserId == 1)).Returns(cust);
+
+            var customerManager = new CustomerManager(mockCustomer.Object);
+
+            // Act
+            var result = customerManager.GetById(1);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual("Customer listed successfully.", result.Message);
+            Assert.AreEqual(1, result.Data.UserId); 
         }
     }
 }
