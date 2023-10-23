e7d413e4daa5db5096065426e881ac4c8e01d0dc
Kaiser VanGuardia <gvanguardia@seattleu.edu>
2023-10-22 18:21:19 -0700

    Added unit tests for get by id for CustomersControllerTests.cs file

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
@@ -3,12 +3,14 @@
 // Purpose: Create Unit tests for Login & Regsiter functionality in ColorsController.cs
 
 using System;
+using Core.Utilities.Result;
 using Business.Abstract;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using WebAPI.Controllers;
+using AutoMapper;
 
 namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
 {
@@ -16,40 +18,50 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
     public class ColorsControllerTests
     {
        
-        [TestMethod]
-        public void Test_GetBy_Id()
-        {
-            // Arrange
-            var mockColorService = new Mock<IColorService>();
-            mockColorService.Setup(x => x.GetById(It.IsAny<int>())).Returns(new ColorResponse(true, "Success", new Color()));
-
-            var controller = new ColorsController(mockColorService.Object);
-
-            // Act
-            var result = controller.GetById(1) as OkObjectResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(400, result.StatusCode);
-           
-        }
-        [TestMethod]
-        public void Test_Get_All()
-        {
-            // Arrange
-            var mockColorService = new Mock<IColorService>();
-            mockColorService.Setup(x => x.GetAll()).Returns(new ColorListResponse(true, "Success", new List<Color>()));
-
-            var controller = new ColorsController(mockColorService.Object);
-
-            // Act
-            var result = controller.GetAll() as OkObjectResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(400, result.StatusCode);
-        }
+       
+        private readonly Mock<IColorService> _colorServiceMock;
         
+        private readonly ColorsController _controller;
+
+
+
+        public ColorsControllerTests()
+        {
+            _colorServiceMock = new Mock<IColorService>();
+            
+            _controller = new ColorsController(_colorServiceMock.Object);
+        }
+        [TestMethod]
+        public void When_Color_Exists_GetById_Returns_OK()
+        {
+            // Arrange
+            int id = 1;
+            var color = new Color { Id = id, Name = "Color1" };
+            var serviceResult = new SuccessDataResult<Color>(color);
+            _colorServiceMock.Setup(service => service.GetById(id)).Returns(serviceResult);
+
+            // Act
+            IActionResult result = _controller.GetById(id);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        }
+
+        [TestMethod]
+        public void When_Color_Does_Not_Exist_GetById_Returns_BadRequestResult()
+        {
+            // Arrange
+            int id = 1;
+            var color = new Color { Id = id, Name = "Color1" };
+            var serviceResult = new ErrorDataResult<Color>(color);
+            _colorServiceMock.Setup(service => service.GetById(id)).Returns(serviceResult);
+
+            // Act
+            IActionResult result = _controller.GetById(id);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
 
     }
 }
