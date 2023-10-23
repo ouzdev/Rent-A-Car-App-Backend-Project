3d8c30789cbb7877ae4f6dbba4ee9fdd66f20dcd
Ajer Lodhi <lodhia@seattleu.edu>
2023-10-23 07:48:43 +0500

    Fixed minor issue and CustomersControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/CustomersControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/CustomersControllerTests.cs
@@ -12,6 +12,9 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
     [TestClass]
     public class CustomersControllerTests
     {
-       
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
     }
 }
