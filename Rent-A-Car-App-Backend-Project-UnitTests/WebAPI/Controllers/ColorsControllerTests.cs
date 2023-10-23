9a09ba60081137440c290acd45ee434fd7490787
Kaiser VanGuardia <gvanguardia@seattleu.edu>
2023-10-22 18:40:22 -0700

    Add unit test for get all for ColorsControllerTests.cs

--- Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/Controllers/ColorsControllerTests.cs
@@ -11,6 +11,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using WebAPI.Controllers;
 using AutoMapper;
+using System.Runtime.InteropServices;
 
 namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
 {
@@ -19,7 +20,7 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
     {
        
        
-        private readonly Mock<IColorService> _colorServiceMock;
+        private readonly Mock<IColorService> _ColorServiceMock;
         
         private readonly ColorsController _controller;
 
@@ -27,18 +28,18 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
 
         public ColorsControllerTests()
         {
-            _colorServiceMock = new Mock<IColorService>();
+            _ColorServiceMock = new Mock<IColorService>();
             
-            _controller = new ColorsController(_colorServiceMock.Object);
+            _controller = new ColorsController(_ColorServiceMock.Object);
         }
         [TestMethod]
         public void When_Color_Exists_GetById_Returns_OK()
         {
             // Arrange
             int id = 1;
-            var color = new Color { Id = id, Name = "Color1" };
-            var serviceResult = new SuccessDataResult<Color>(color);
-            _colorServiceMock.Setup(service => service.GetById(id)).Returns(serviceResult);
+            var Color = new Color { Id = id, Name = "Color1" };
+            var serviceResult = new SuccessDataResult<Color>(Color);
+            _ColorServiceMock.Setup(service => service.GetById(id)).Returns(serviceResult);
 
             // Act
             IActionResult result = _controller.GetById(id);
@@ -52,9 +53,9 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
         {
             // Arrange
             int id = 1;
-            var color = new Color { Id = id, Name = "Color1" };
-            var serviceResult = new ErrorDataResult<Color>(color);
-            _colorServiceMock.Setup(service => service.GetById(id)).Returns(serviceResult);
+            var Color = new Color { Id = id, Name = "Color1" };
+            var serviceResult = new ErrorDataResult<Color>(Color);
+            _ColorServiceMock.Setup(service => service.GetById(id)).Returns(serviceResult);
 
             // Act
             IActionResult result = _controller.GetById(id);
@@ -62,6 +63,88 @@ namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI.Controllers
             // Assert
             Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
         }
+
+ 
+
+
+
+        #region GetAll
+
+        [TestMethod]
+
+        public void When_Colors_Exist_GetAll_Returns_OK()
+
+        {
+
+            // Arrange
+
+            var Colors = new List<Color>
+
+            {
+
+                new Color { Id = 0, Name = "B0" },
+
+                new Color { Id = 1, Name = "B1" },
+
+            };
+
+            var serviceResult = new SuccessDataResult<List<Color>>(Colors);
+
+            _ColorServiceMock.Setup(service => service.GetAll()).Returns(serviceResult);
+
+
+
+            // Act
+
+            IActionResult result = _controller.GetAll();
+
+
+
+            // Assert
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+        }
+
+
+
+        [TestMethod]
+
+        public void When_Colors_Do_Not_Exist_GetAll_Returns_BadRequest()
+
+        {
+
+            // Arrange
+
+            var Colors = new List<Color>
+
+            {
+
+                new Color { Id = 0, Name = "B0" },
+
+                new Color { Id = 1, Name = "B1" },
+
+            };
+
+            var serviceResult = new ErrorDataResult<List<Color>>(Colors);
+
+            _ColorServiceMock.Setup(service => service.GetAll()).Returns(serviceResult);
+
+
+
+            // Act
+
+            IActionResult result = _controller.GetAll();
+
+
+
+            // Assert
+
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+
+        }
+
+        #endregion
 
     }
 }
