03d57809b827abfd4c92b7867795cb827ebdf35b
Ajer Lodhi <lodhia@seattleu.edu>
2023-11-24 08:42:43 +0500

    Added unit tests to ProgramTests.cs and StartupTests.cs

---
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/ProgramTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using WebAPI;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Creating_Host_Builder_Should_Configure_Host_Builder()
+        {
+            // Arrange
+            var args = new string[] {};
+
+            // Act
+            var hostBuilder = Program.CreateHostBuilder(args);
+
+            // Assert
+            Assert.IsNotNull(hostBuilder);
+        }
+
+    }
+}
+
---
+++ Rent-A-Car-App-Backend-Project-UnitTests/WebAPI/StartupTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebAPI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using WebAPI.Controllers;
+
+namespace Rent_A_Car_App_Backend_Project_UnitTests.WebAPI
+{
+    [TestClass]
+    public class StartupTests
+    {
+        [TestMethod]
+        public void ConfigureServices_ShouldConfigureServices()
+        {
+            // Arrange
+            var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string> {})
+            .Build();
+            var services = new ServiceCollection();
+            var startup = new Startup(configuration);
+
+            // Act
+            startup.ConfigureServices(services);
+            
+            // Assert
+            var serviceProvider = services.BuildServiceProvider();
+            var controller = serviceProvider.GetService<CustomersController>();
+            Assert.IsNull(controller);
+        }
+
+        [TestMethod]
+        public void Configure_ShouldConfigureMiddleware()
+        {
+            // Arrange
+            var app = new Mock<IApplicationBuilder>();
+            var env = new Mock<IWebHostEnvironment>();
+            var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string> { /* Add configuration key-value pairs */ })
+            .Build();
+            var services = new ServiceCollection();
+            var serviceProvider = services.BuildServiceProvider();
+            var startup = new Startup(configuration);
+
+            // Act
+            startup.Configure(app.Object, env.Object);
+
+            // Assert
+            app.Verify(a => a.UseDeveloperExceptionPage(), Times.Never());
+        }
+    }
+}
+
